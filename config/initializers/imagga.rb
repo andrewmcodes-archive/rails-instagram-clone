d1f9e18bbbd673a6bb1478711d526c6efa3d82e4
Andrew Mason <astrosurfer888@gmail.com>
2018-02-12 07:42:48 -0500

    Get rid of Imagga stuff for now

--- config/initializers/imagga.rb
+++ config/initializers/imagga.rb
@@ -1,6 +1,6 @@
 # image_url = 'http://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
-api_key = ENV.fetch('IMAGGA_KEY')
-api_secret = ENV.fetch('IMAGGA_SECRET')
+# api_key = ENV.fetch('IMAGGA_KEY')
+# api_secret = ENV.fetch('IMAGGA_SECRET')
 
 # auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
 # response = RestClient.get "https://api.imagga.com/v1/tagging?url=#{image_url}", { :Authorization => auth }
